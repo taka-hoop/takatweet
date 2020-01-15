a33a24c96c6cb4a21751e4ec4ec93269b82972ee
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2020-01-15 19:02:01 +0900

    cerate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :tweets, only: :index
+  resources :tweets, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
