20a20d39d9edf0ea43797cbc981df711219ad6eb
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2020-01-15 19:32:14 +0900

    edit update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "tweets#index"
-  resources :tweets, only: [:index, :new, :create, :destroy]
+  resources :tweets, only: [:index, :new, :create, :destroy, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
