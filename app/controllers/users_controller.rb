4b95131593d8a14eb1ccbd02b5e14a0d8ab3aa56
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2020-01-16 17:41:23 +0900

    fin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = current_user.nickname
-    @tweets = current_user.tweets
+    @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
   end
 end
